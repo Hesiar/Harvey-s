4b7ceceb12a9546ab27c8822a6f5217e2742692d
Armando <hesiar94@gmail.com>
2025-04-14 11:43:32 +0200

    Maquetación. Rehacer CSS para que funcione en PHP

---
+++ header.php
@@ -0,0 +1,41 @@
+<header>     
+    <div>
+        <a href="/Harvey-s/maqueta.html">
+            <img src="/Harvey-s/elementos/Harveys_logo.png" alt="Logo de Harvey's">
+        </a>
+
+        <button class="secciones" type="button">
+            <i class="fas fa-bars"></i>
+            Secciones
+        </button>
+
+        <form id="busqueda" action="/Harvey-s/buscar.php" method="get">
+            <input class="campo-busqueda" type="text" placeholder="Buscar en Harvey's" name="search" required>
+            <button class="btn-busqueda" type="submit">
+                <i class="fas fa-search"></i>
+            </button>
+        </form>
+
+        <form action="/Harvey-s/fqa.html">
+            <button class="btn-fqa" type="submit">
+                <i class="fas fa-question-circle"></i>
+            </button>
+        </form>
+
+        <form action="/Harvey-s/login.html">
+            <button class="btn-login" type="submit">
+                <i class="fas fa-user"></i>
+                Iniciar sesión
+            </button>
+        </form>
+
+        <form action="/Harvey-s/carrito.html">
+            <button class="btn-carrito" type="submit">
+                <i class="fas fa-shopping-cart"></i>
+                Carrito
+            </button>
+        </form>
+    </div>
+
+    <hr>
+</header>
---
+++ maqueta.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="elementos/css_clientes.css">
+    <link rel="icon" href="elementos/icon.ico" type="image/x-icon">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css"> <!-- Iconos de Font Awesome -->
+    <title>Harvey's</title>
+</head>
+<body>
+    <?php include 'header.php'; ?>  
+    
+
+</body>
+</html>
